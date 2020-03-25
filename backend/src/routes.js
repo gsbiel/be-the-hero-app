dcc3fdadbca5b05c1fce3e48879d814b1da2fc04
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-03-25 17:11:01 -0300

    Refatora codigo, cria OngController para organizar as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,29 +1,8 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
-
+const {create, list} = require('./controllers/OngController');
 const routes = express.Router();
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*'); 
-    return response.json(ongs);
-});
-
-routes.post("/ongs", async (request, response) => {
-    const {name, email, whatsapp, city, uf} = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-    
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
-
-    return response.json({id});
-});
+routes.get('/ongs', list);
+routes.post("/ongs", create );
 
 module.exports = routes;
