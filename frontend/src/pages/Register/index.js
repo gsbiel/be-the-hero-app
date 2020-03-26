e22e08db82b1267a7f8c22788c20155135cb096d
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-03-26 14:32:49 -0300

    Cria componente Profile e implementa rotapara ele

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -15,10 +15,23 @@ const Register = (props) => {
                     <img src = {logoImg} alt = "Be the Hero"/>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude as pessoas a encontrarem os casos da sua ONG.</p>
-                    <Link className = "back-link" to = "/register">
-                        <FiArrowLeft size={16} color="#E02041"/> Não tenho cadastro
+                    <Link className = "back-link" to = "/">
+                        <FiArrowLeft size={16} color="#E02041"/> Voltar
                     </Link>
                 </section>
+
+                <form>
+                    <input placeholder="Nome da ONG" /> 
+                    <input type="email" placeholder="Email" />
+                    <input placeholder="Whatsapp" />
+
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{width: 80}} />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
 
             </div>
         </div>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,11 +7,13 @@ import {
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 const Routes = (props) => {
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/profile" component={Profile} />
                 <Route path="/register" component={Register} />
                 <Route path="/" component={Logon} />  
             </Switch>
