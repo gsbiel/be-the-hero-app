24104d07bbd426396dfd390823d557f929126e2e
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-03-26 14:25:35 -0300

    implementa e estiliza component Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import {
+    FiArrowLeft
+} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
@@ -12,6 +15,9 @@ const Register = (props) => {
                     <img src = {logoImg} alt = "Be the Hero"/>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude as pessoas a encontrarem os casos da sua ONG.</p>
+                    <Link className = "back-link" to = "/register">
+                        <FiArrowLeft size={16} color="#E02041"/> Não tenho cadastro
+                    </Link>
                 </section>
 
             </div>
