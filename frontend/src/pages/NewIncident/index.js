e8d67586634d49cae920081b87d4a64ac86e775a
Gabriel <gabriel.sgaspar@yahoo.com.br>
2020-03-26 16:41:24 -0300

    Cria componente NewIncident e implementa rota para ele

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const newIncident = (props) => {
+    return(
+        <h1>Página de cadastro de novos incidentes!</h1>
+    );
+}
+
+export default newIncident;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,11 +8,13 @@ import {
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 const Routes = (props) => {
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/incidents/new" component={NewIncident} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/register" component={Register} />
                 <Route path="/" component={Logon} />  
